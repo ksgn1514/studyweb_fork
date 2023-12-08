31490b18e98280f19a635f4af8bb0edc66b23603
WEAVUS-SURFACE\clils <clilseoo2@gmail.com>
2023-12-08 19:24:12 +0900

    controoler

--- src/main/java/com/weavus/studyweb/controller/InitController.java
+++ src/main/java/com/weavus/studyweb/controller/InitController.java
@@ -6,9 +6,48 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class InitController {
 
-@GetMapping("/")
+    @GetMapping("/")
     private String init() {
 
-    return "index";
+        return "index";
     }
+
+    @GetMapping("/login")
+    private String login() {
+
+        return "login";
+    }
+
+    @GetMapping("/join")
+    private String join() {
+
+        return "join";
+    }
+
+    //
+    @GetMapping("/studys")
+    private String studys() {
+
+        return "studys";
+    }
+    @GetMapping("/community")
+    private String community() {
+
+        return "community";
+    }
+
+    @GetMapping("/studyenroll")
+    private String studyenroll() {
+
+        return "studyenroll";
+    }
+
+    @GetMapping("/studydetail")
+    private String studydetail() {
+
+        return "studydetail";
+    }
+
+
+
 }
